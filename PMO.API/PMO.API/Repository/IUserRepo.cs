aa1e8a8529bf8a8a4f1eaf630a74d71d9c64bf2a
VenkateshSrinivasan <venkatesh-5.srinivasan-5@cognizant.com>
2020-04-17 14:51:10 +0530

    Added UserSearchCriteria and User Repo

--- PMO.API/PMO.API/Repository/IUserRepo.cs
+++ PMO.API/PMO.API/Repository/IUserRepo.cs
@@ -1,4 +1,5 @@
 ﻿using PMO.API.DomainModel;
+using PMO.API.Messages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace PMO.API.Repository
         public Task<Tuple<bool, string>> AddUser(PMOUser pmoUser);
         public Task<Tuple<bool, string>> EditUser(PMOUser pmoUser);
         public Task<List<PMOUser>> GetAllUser();
+        public Task<List<PMOUser>> GetAllUserMatchAnyCriteria(UserSearchCriteria userSearchCriteria);
         public Task<PMOUser> GetUserByEmployeeId(string employeeId);
     }
 }
