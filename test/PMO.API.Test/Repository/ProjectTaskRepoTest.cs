458cb67a8546e51253fa0638330feadcd20112cb
VenkateshSrinivasan <venkatesh-5.srinivasan-5@cognizant.com>
2020-04-20 10:12:35 +0530

    Excepte Suspend and EndTask rest other test are passing

--- test/PMO.API.Test/Repository/ProjectTaskRepoTest.cs
+++ test/PMO.API.Test/Repository/ProjectTaskRepoTest.cs
@@ -81,11 +81,12 @@ namespace PMO.API.Test.Repository
             }
         }
         [Fact]
-        public async Task CUDProjectTest()
+        public async Task CUProjectTaskTest()
         {
+            
             var project = new Project {
                 End = DateTime.Today.AddDays(1),
-                PMId="E001",
+                PMId="EP001",
                 Priority=1,
                 Start=DateTime.Today,
                 Status = 0,
@@ -94,10 +95,76 @@ namespace PMO.API.Test.Repository
             var logger = createProjectTaskLogger();
             using (var session = DocStore.OpenAsyncSession())
             {
+                var usrlogger = createUserLogger();
+                var userRepo = new UserRepo(session, usrlogger);
                 var projTaskRepo = new ProjectTaskRepo(session, logger);
+                
                 var projAddResult = await projTaskRepo.AddProject(project);
                 Assert.True(projAddResult.Item1);
                 Assert.NotEmpty(projAddResult.Item2);
+                
+                var projectMod = new Project
+                {
+                    End = DateTime.Today.AddDays(1),
+                    PMId = ((await userRepo.GetUserByEmployeeId("EP001"))?.Id),
+                    Priority = 1,
+                    Start = DateTime.Today,
+                    Status = 0,
+                    Title = "Project A1",
+                    Id = projAddResult.Item2
+                };
+                var projEditResult = await projTaskRepo.EditProject(projectMod);
+                Assert.True(projEditResult.Item1);
+                Assert.NotEmpty(projEditResult.Item2);
+                
+                var task1 = new ProjTask {
+                    EndDate = DateTime.Today.AddDays(2),
+                    Name = "Task 1",
+                    Priority=1,
+                    Start=DateTime.Today,
+                    Status=0,
+                    TaskOwnerId= ((await userRepo.GetUserByEmployeeId("EP002"))?.Id)
+                };
+                var projTskAddResult = await projTaskRepo.AddTask(projEditResult.Item2, task1);
+                Assert.True(projTskAddResult.Item1);
+                Assert.NotEmpty(projTskAddResult.Item2);
+               
+                var taskChild1 = new ProjTask
+                {
+                    EndDate = DateTime.Today.AddDays(2),
+                    Name = "Task 1-1",
+                    Priority = 1,
+                    Start = DateTime.Today,
+                    Status = 0,
+                    TaskOwnerId = ((await userRepo.GetUserByEmployeeId("EP003"))?.Id),
+                    ParentTaskId= projTskAddResult.Item2
+                };
+                var projTskChldAddResult = await projTaskRepo.AddTask(projEditResult.Item2, taskChild1);
+                Assert.NotEmpty(projTskChldAddResult.Item2);
+              
+                var taskMod = new ProjTask
+                {
+                    EndDate = DateTime.Today.AddDays(2),
+                    Name = "Task A1",
+                    Priority = 1,
+                    Start = DateTime.Today,
+                    Status = 0,
+                    TaskOwnerId = ((await userRepo.GetUserByEmployeeId("EP002"))?.Id),
+                    Id = "P/1-1"
+                };
+                var projTskEditResult = await projTaskRepo.EditTask("P/1", taskMod);
+                Assert.True(projTskAddResult.Item1);
+                Assert.NotEmpty(projTskAddResult.Item2);
+                
+                var projTaskGetAllTask = await projTaskRepo.GetAllActiveTask("P/1");
+                Assert.NotEmpty(projTaskGetAllTask);
+                
+                var projGetProjByName = await projTaskRepo.GetProjectByName("A1");
+                Assert.NotEmpty(projGetProjByName);
+
+                var projTaskGetAllActiveProjects = await projTaskRepo.GetAllActiveProject();
+                Assert.NotEmpty(projTaskGetAllActiveProjects);
+
             }
 
         }
