aaaf4ea221481d5d5bb8bc409792569ef1704b05
VenkateshSrinivasan <venkatesh-5.srinivasan-5@cognizant.com>
2020-04-27 14:42:16 +0530

    DateConverter modified to handle culture invariant formats

--- src/PMO.API/Converter/DateTimeConverter.cs
+++ src/PMO.API/Converter/DateTimeConverter.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace PMO.API.Converter
 {
@@ -13,11 +14,19 @@ namespace PMO.API.Converter
     {
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            if (reader.HasValueSequence)
-
-                return DateTime.Parse(reader.GetString());
-            else
+            var cultureInfo = new CultureInfo("");
+            cultureInfo.DateTimeFormat.ShortDatePattern = "yyyy-MM-dd";
+            var strDate = reader.GetString();
+            if (string.IsNullOrWhiteSpace(strDate))
                 return DateTime.MinValue;
+            else
+                return DateTime.Parse(strDate, cultureInfo.DateTimeFormat);
+
+            //if (reader.TokenType
+
+                //    return DateTime.Parse(reader.GetString());
+                //else
+                //    return DateTime.MinValue;
         }
 
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
